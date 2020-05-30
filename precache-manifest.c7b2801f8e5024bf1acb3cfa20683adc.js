81dc652ce8d79139c34c6eed734fc681ddbb3eaa
Utkarsh1329 <utkarshpateriya.78@gmail.com>
2020-05-30 19:20:59 +0530

    Updates

---
+++ precache-manifest.c7b2801f8e5024bf1acb3cfa20683adc.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a79523a0358d7adf7b6d4ba45c5328",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "d9a8f52b275ec7f875ab",
+    "url": "/profile/static/css/main.4decc217.chunk.css"
+  },
+  {
+    "revision": "de386ef2b49b0ba16c16",
+    "url": "/profile/static/js/2.292ffedf.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/profile/static/js/2.292ffedf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a8f52b275ec7f875ab",
+    "url": "/profile/static/js/main.7bc221c8.chunk.js"
+  },
+  {
+    "revision": "ea45762903b15269b24a",
+    "url": "/profile/static/js/runtime-main.6a573ec8.js"
+  },
+  {
+    "revision": "2345a5c4d363dd55b0d5904d0d0c9087",
+    "url": "/profile/static/media/angular.2345a5c4.png"
+  },
+  {
+    "revision": "21b2dd2898aa56c94575b171e6a59edd",
+    "url": "/profile/static/media/blender.21b2dd28.png"
+  },
+  {
+    "revision": "87d994875c22d91270c9d27302a45fb7",
+    "url": "/profile/static/media/bootstrap.87d99487.png"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/profile/static/media/cpp.f278be79.png"
+  },
+  {
+    "revision": "64e97dc79ed967728e336e558c813547",
+    "url": "/profile/static/media/dart.64e97dc7.png"
+  },
+  {
+    "revision": "a3492ddba4b979effdc4c62f5adf1bb2",
+    "url": "/profile/static/media/html5.a3492ddb.svg"
+  },
+  {
+    "revision": "267657e869522c9e21eb501616a70d06",
+    "url": "/profile/static/media/materialize.267657e8.png"
+  },
+  {
+    "revision": "020e80ec696f6e10fd6671c3d8edd365",
+    "url": "/profile/static/media/photoshop.020e80ec.png"
+  },
+  {
+    "revision": "44403f1eece67c892a0c6a6791da5ad4",
+    "url": "/profile/static/media/php.44403f1e.svg"
+  },
+  {
+    "revision": "b9bbb1d722d3ef6ebf7d573703b5925e",
+    "url": "/profile/static/media/py.b9bbb1d7.png"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/profile/static/media/react.8d859602.png"
+  },
+  {
+    "revision": "c13c393f310c02a422138de8d2ba4f31",
+    "url": "/profile/static/media/sass.c13c393f.png"
+  },
+  {
+    "revision": "6ded14c8a9e078f1d9c305186271078d",
+    "url": "/profile/static/media/wordpress.6ded14c8.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a095b9d428fa1bfb6c102f61837a155.js"
+  "/profile/precache-manifest.c7b2801f8e5024bf1acb3cfa20683adc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
