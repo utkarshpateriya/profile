99daf873283f3f41b24558d54af0f02aa529e1e1
Utkarsh1329 <utkarshpateriya.78@gmail.com>
2020-06-04 11:15:29 +0530

    final commit

--- src/components/contactElements/contactBody.js
+++ src/components/contactElements/contactBody.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Divider, Grid, Paper } from '@material-ui/core';
+import { makeStyles} from '@material-ui/core';
 import Background from './../homeElements/backgroundImg'
 import Footer from './../homeElements/footer';
 
